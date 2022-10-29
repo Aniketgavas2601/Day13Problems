846efbc30adc6892adfbc215b84244c562e5d861
Experiment2601 <aniketgavas18@gmail.com>
2022-10-29 11:13:58 +0530

    Maximum Float Number

--- MaximumNumberTest.java
+++ MaximumNumberTest.java
@@ -25,4 +25,25 @@ public class MaximumNumberTest {
         MaximumNumber maximumNumber = new MaximumNumber(26,34,45);
         Assert.assertEquals(45,maximumNumber.maximum());
     }
+
+    //TC 2.1 - Given Maximum  Float Number at First Position return the Same Number
+    @Test
+    public void getMaximumFloatNumberAtFirstPosition(){
+        MaximumNumber maximumFloatNumber = new MaximumNumber(4.5f, 2.3f, 3.3f);
+        Assert.assertEquals(4.5f, maximumFloatNumber.maximum());
+    }
+
+    //TC 2.2 - Given Maximum  Float Number at Second Position return the Same Number
+    @Test
+    public void getMaximumFloatNumberAtSecondPosition(){
+        MaximumNumber maximumFloatNumber = new MaximumNumber(2.3f, 4.5f, 3.3f);
+        Assert.assertEquals(4.5f, maximumFloatNumber.maximum());
+    }
+
+    //TC 2.3 - Given Maximum  Float Number at Third Position return the Same Number
+    @Test
+    public void getMaximumFloatNumberAtThirdPosition(){
+        MaximumNumber maximumFloatNumber = new MaximumNumber(3.3f, 2.3f, 4.5f);
+        Assert.assertEquals(4.5f,maximumFloatNumber.maximum());
+    }
 }
